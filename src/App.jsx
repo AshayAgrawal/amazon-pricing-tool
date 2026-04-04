f915a716f72d85937d215f36b629cbb825f91942
Ashay Agrawal <ashay100@gmail.com>
2026-04-04 21:24:48 +0530

    Add basic App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{padding: 20}}>
+      <h1>Amazon Pricing Tool</h1>
+      <p>Your app is now connected. Replace this with your pricing logic.</p>
+    </div>
+  )
+}
